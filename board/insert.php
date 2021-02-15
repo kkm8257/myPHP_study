39ddfd1aaba220dadf901cf503092f5973709158
kkm8257 <tenman2@naver.com>
2021-02-15 16:56:48 +0900

    ch 9

--- board/insert.php
+++ board/insert.php
@@ -18,8 +18,29 @@
     $user_name=$_POST['name'];
     $user_msg=$_POST['message'];
 
-    print $user_name."<br>";
-    print $user_msg;
+    // print $user_name."<br>";
+    // print $user_msg;
+
+
+//https://dev.mysql.com/doc/refman/8.0/en/sql-data-manipulation-statements.html
+//crud 관련 정보
+
+
+    $sql = "INSERT INTO msg_board(name,message) VALUES('$user_name','$user_msg')";
+    $result= mysqli_query($conn,$sql);
+
+    //리턴값은 true or false
+
+    if($result===false){
+        echo '저장하지 못 했습니다.';
+        error_log(mysqli_error($conn));//에러로그기록
+    }else{
+
+        echo '저장 성공';
+
+    }
+
+
 
     mysqli_close($conn);
     print "<hr/><a href='index.html'>메인화면으로 이동하기</a>";
